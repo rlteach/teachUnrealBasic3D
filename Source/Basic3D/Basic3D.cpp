57d296bd53b03bcde26ddcc3c4f648534ee699bb
Richard Leinfellner <richard@leinfellner.com>
2017-09-08 15:00:22 +0100

    First Upload

---
+++ Source/Basic3D/Basic3D.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Basic3D.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Basic3D, "Basic3D" );
---
+++ Source/Basic3D/Basic3DGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Basic3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASIC3D_API ABasic3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
